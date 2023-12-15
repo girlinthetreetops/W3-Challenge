7c5f5d29eb9e882d35afa00a434aded1c0b54b82
girlinthetreetops <andrea.stykket@gmail.com>
2023-12-15 11:10:55 +0000

    Initial Commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogSpawnTimer;
+    private float minimumInterval = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        dogSpawnTimer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dogSpawnTimer >= minimumInterval)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                dogSpawnTimer = 0;
+            }
+
+            
+        }
+    }
+}
